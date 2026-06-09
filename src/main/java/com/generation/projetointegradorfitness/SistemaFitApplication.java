e3f89c03186931e921376cce1ae50bc437515b2a
GimendescCodes <giovannamendessenai@gmail.com>
2026-06-09 14:44:10 -0300

    first commit

---
+++ src/main/java/com/generation/projetointegradorfitness/SistemaFitApplication.java
@@ -0,0 +1,13 @@
+package com.generation.projetointegradorfitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaFitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/projetointegradorfitness/SistemaFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.projetointegradorfitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
